907ed1df5954bf341353265248647178fdffe2e0
Vlad-Drishchuk <vlad.drishchuk@gmail.com>
2022-11-12 17:34:46 +0200

    Initial commit

---
+++ lab1_1.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "math.h"
+#include "locale.h"
+
+
+int main(){
+    setlocale(LC_CTYPE, "ukr");
+    double a, b, c, x, m;
+    printf("Ведіть сторону A:");
+    scanf("%lf", &a);
+    printf("\nВведіть сторону B: ");
+    scanf("%lf", &b);
+    printf("\nВедіть сторону C: ");
+    scanf("%lf", &c);
+
+}
